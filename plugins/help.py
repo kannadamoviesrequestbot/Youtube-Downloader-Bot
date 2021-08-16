481511ba42891cd020853cdf34e3936f669528dd
kannadamoviesrequestbot <88633158+kannadamoviesrequestbot@users.noreply.github.com>
2021-08-17 00:19:46 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,7 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) please send me Send Youtube Url
-JOIN MY CHANNEL
-@KANNADAMOVIES_17"
+    helptxt = f"Currently Only supports Youtube Single  (No playlist) please send me Send Youtube Url JOIN MY CHANNEL @KANNADAMOVIES_17"
     await message.reply_text(helptxt)
