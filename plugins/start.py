19040f58458b3e64d82ef6d8aa3d9fa021be1f0e
kannadamoviesrequestbot <88633158+kannadamoviesrequestbot@users.noreply.github.com>
2021-08-17 00:09:16 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Channel", url="https://t.me/KANNADAMOVIES_17")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/beereshbanakards")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey hello 👋<b>{message.from_user.first_name} PLEASE JOIN MY CHANNEL @KANNADAMOVIES_17 </b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
