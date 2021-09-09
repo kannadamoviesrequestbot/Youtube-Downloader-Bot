7fec51a1daf49929bde567281150187c356ace80
kannadamoviesrequestbot <88633158+kannadamoviesrequestbot@users.noreply.github.com>
2021-09-09 16:58:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,7 +5,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/KANNADAMOVIES_17")],
+        [InlineKeyboardButton("Channel", url="https://t.me/BAGURUJOINAGUUKANNADAMOVIES_17")],
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/beereshbanakards")]
     ])
